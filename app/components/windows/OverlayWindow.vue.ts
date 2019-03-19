4d84f3c0801e429fd09a0d2ceed36a2f3c48258b
Adrian Perez <adrian@adrianperez.org>
2019-03-19 15:50:33 -0700

    refactor(overlay): optimize imports on overlay controls win

--- app/components/windows/OverlayWindow.vue.ts
+++ app/components/windows/OverlayWindow.vue.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
+import { Inject } from 'util/injector';
+import { GameOverlayService } from 'services/game-overlay';
+import { StreamInfoService } from 'services/stream-info';
 import ModalLayout from '../ModalLayout.vue';
-import { StreamInfoService } from '../../services/stream-info';
-import { Inject } from '../../util/injector';
 import SceneSelector from '../SceneSelector.vue';
 import Display from 'components/shared/Display.vue';
 import StartStreamingButton from '../StartStreamingButton.vue';
-import { GameOverlayService } from '../../services/game-overlay';
 
 @Component({ components: { Display, ModalLayout, SceneSelector, StartStreamingButton } })
 export default class OverlayWindow extends Vue {
