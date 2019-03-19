cae74a10d1542540729e9aa48d12730183db5423
Adrian Perez <adrian@adrianperez.org>
2019-03-19 16:00:15 -0700

    refactor(overlay): re-enable `contextIsolation` for views
    As we moved away from `BrowserView` we removed their preferences and `contextIsolation` was not
    added to the respective `BrowserWindow`'s. While we control one of sources (streamlabs.com) and the
    other one is Twitch Chat, there shouldn't be any issues, but this protects us regardless.

--- app/services/game-overlay/index.ts
+++ app/services/game-overlay/index.ts
@@ -109,6 +109,7 @@ export class GameOverlayService extends PersistentStatefulService<GameOverlaySta
       thickFrame: false,
       webPreferences: {
         nodeIntegration: false,
+        contextIsolation: true,
       },
     };
 
